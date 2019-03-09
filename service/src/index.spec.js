5d13fa6bb3e2448bb88bf527ac21b92699bc0a68
Vlad Dubrovskis <vlad@vovka.co.uk>
2019-03-09 15:10:21 +0000

    Add supertest and udpate the initial test for the service

--- service/src/index.spec.js
+++ service/src/index.spec.js
@@ -1,11 +1,19 @@
-const Koa = require('koa');
 const { expect } = require('chai');
+const request = require('supertest');
 
 const server = require('./index');
 
+let instance;
+
 describe('index', () => {
-  it('should create an instance of a Koa server', () => {
-    const instance = server();
-    expect(instance).to.be.instanceof(Koa);
+  before(() => { instance = server().listen(process.env.TEST_SERVER_PORT || 8888) })
+  after(() => { instance.close() })
+
+  it('should have return welcome message on root path', async () => {
+    const response = await request(instance).get("/");
+    expect(response.status).to.equal(200);
+    expect(response.text).to.equal('Hello world');
   });
+
+
 });
