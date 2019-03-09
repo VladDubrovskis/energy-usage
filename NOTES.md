78ed5dfd046e4faa1264479279eabfb8a30d430a
Vlad Dubrovskis <vlad@vovka.co.uk>
2019-03-09 17:56:17 +0000

    Add a basic implementation of an estimate algorithm - with more time can probably improve it - but it works

--- NOTES.md
+++ NOTES.md
@@ -9,4 +9,5 @@ Notes:
 - Could add some valdiation - e.g. if the user attempts to submit a reading that is lower, or if there is already a reading this month (e.g. based on the assumptions in README)
 - Could also validate payload and return 400 if invalid
 - Specifically went with supertest as wanted to test the behaviour and not the implementation. If the application would be a lot mre resource heavy may have approached the testing in a different way.
-
+- Calculation for the estimate will be done based on idea that last 2 entries are from current and previous month. In real application probably would validate that before estimation
+- Also would probably make the current request date play some role in decision making, or make it a parameter 
--- service/src/estimator.spec.js
+++ service/src/estimator.spec.js
@@ -2,8 +2,31 @@ const { expect } = require('chai');
 
 const { calculate } = require('./estimator');
 
+
+const previousMonth = {
+  cumulative: 20406,
+  reading_date: '2018-03-14T00:00:00.000Z',
+  unit: 'kWh',
+};
+
+const latestMeterReading = {
+  cumulative: 20750,
+  reading_date: '2018-04-29T00:00:00.000Z',
+  unit: 'kWh',
+};
+
+const latestMeterReadingEndOfTheMonth = {
+  cumulative: 20750,
+  reading_date: '2018-04-30T00:00:00.000Z',
+  unit: 'kWh',
+};
+
 describe('data', () => {
-  it('passes sanity check', () => {
-    expect(calculate()).to.equal(true);
+  it('calculates end of the month estimate', () => {
+    expect(calculate(latestMeterReading, previousMonth)).to.equal(352);
+  });
+
+  it('correctly calculates estimate if last reading was on the last day of the month', () => {
+    expect(calculate(latestMeterReadingEndOfTheMonth, previousMonth)).to.equal(344);
   });
 });
